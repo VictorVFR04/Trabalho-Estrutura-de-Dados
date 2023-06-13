55016e082717939ed931f49f92c5fafaddfb1f38
Victor Ramos <120141481+VictorVFR04@users.noreply.github.com>
2023-06-13 16:23:26 -0300

    Criado vetor

--- Trabalho_2_Bimestre/src/main/java/com/mycompany/trabalho_2_bimestre/Ordenacoes.java
+++ Trabalho_2_Bimestre/src/main/java/com/mycompany/trabalho_2_bimestre/Ordenacoes.java
@@ -3,8 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.trabalho_2_bimestre;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Desenvolvedores:
@@ -12,20 +10,50 @@ import java.util.List;
  * Mateus Martins da Silva (RA: 00231293)
  */
 public class Ordenacoes {
+    int[] numeros = {32,43,1,132,43,5,4,7};
     
-    List<Ordenacoes> ordenacoes = new ArrayList<>();
     
     public void ordenarInsercao (){
         
     }
     
     public void ordenarSelecao (){
-        
+        for(int i = 0; i < numeros.length; i++){
+            int posicaoMenor = i;
+            
+        for(int j = i+1; j < numeros.length; j++){
+            if(numeros[j] < numeros[posicaoMenor]){
+                posicaoMenor = j;
+                }
+            }
+        if(posicaoMenor != i){
+            int aux = numeros[i];
+            numeros [i] = numeros[posicaoMenor];
+            numeros[posicaoMenor] = aux;
+            }
+        }
+        for(int i =0; i < numeros.length; i++){
+            System.out.println("posicao [" +i+ "[ = "+numeros[i]);
+        }
     }
     
     public void ordenarBolha (){
-        
+        boolean mudou = true;
+
+        while(mudou){
+            mudou = false;
+    
+        for (int i = 0; i < numeros.length - 1; i++){
+            if(numeros[i] > numeros[i+1]){
+                int aux = numeros [i];
+                numeros [i] = numeros [i+1];
+                numeros [i+1] = aux; 
+                mudou = true;
+                }
+            }
+        }
+          for(int i = 0; i < numeros.length; i++){
+            System.out.println("posicao ["+i+"] = "+numeros[i]);
+         }
     }
-    
-    
 }
--- Trabalho_2_Bimestre/src/main/java/com/mycompany/trabalho_2_bimestre/Trabalho_2_Bimestre.java
+++ Trabalho_2_Bimestre/src/main/java/com/mycompany/trabalho_2_bimestre/Trabalho_2_Bimestre.java
@@ -11,25 +11,26 @@ import javax.swing.JOptionPane;
  * Mateus Martins da Silva (RA: 00231293)
  */
 public class Trabalho_2_Bimestre {
-
+    
     public static void main(String[] args) {
-
+    Ordenacoes ordenacoes = new Ordenacoes();
+          
     int opcao = Integer.parseInt(JOptionPane.showInputDialog(" (1) Inserir Vetor de Inteiros \n (2) Ordenar por Inserção \n (3) Ordernar por Seleção \n (4) Ordernar por Bolha \n (5) Pesquisar Número"));    
     
-    switch (opcao) {
+     switch (opcao) {
+         case 1: 
+         ordenacoes.ordenarBolha();
+         break;
+          
+         case 2: 
+         ordenacoes.ordenarSelecao();
+         break;
+         
+         case 3:
+         
+     }
 
-        case 1:
-
-        case 2:
-
-        case 3:
-
-        case 4:
-
-        case 5:
-
-}
+   
     }
-    
 }
 
